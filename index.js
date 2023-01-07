abdc9434f235381368ca233923156a3ebcc0e2af
Priyamani2004 <manimegalaulm@gmail.com>
2023-01-07 16:13:53 +0530

    modified:   index.js

--- index.js
+++ index.js
@@ -35,6 +35,8 @@ function getTime()
                    console.log("Timezone : "+values[1]);
                    console.log("Week Number : "+values[13]);
                    let date=(values[6].split(","));
+                   console.log(date[0]);
+                   let days=[""]
                    
                     
                 }
@@ -45,3 +47,22 @@ function getTime()
     
 }
 getTime();
+const randomquote=function(name){
+    fetch(`https://api.api-ninjas.com/v1/quotes?category=${name}`,{
+        method:'GET',
+        headers:{
+            'X-api-key':'tPUFvi5yfYVrTUpgT0l7EA==UYK9CMSmxMnW8cEV'
+        }
+    })
+    .then(function(response){
+       return response.json();
+    })
+     .then(function(data){
+       let quotes=data[0];
+       let sentence=quotes.quote;
+       let author=quotes.author
+      console.log(sentence);
+      console.log(author);
+ })
+}
+ randomquote("success")
