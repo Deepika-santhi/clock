209b0a8815c5d05c4c77df53bf7cd41f3e379369
Amutha09K <amuthakaliyappan03@gmail.com>
2023-01-09 09:42:21 +0530

    commit

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 //f2b0b8b7de284fb284cf2210dd2412fb
 
+
+
+let Time,curTime="",AmOrPm="";
+
 //https://api.ipgeolocation.io/timezone?apiKey=API_KEY&ip=1.1.1.1
 function getTime()
 {
@@ -32,12 +36,43 @@ function getTime()
                    console.log(dat);
                    let values=Object.values(dat);
                    console.log(values);
+<<<<<<< HEAD
                    console.log("Timezone : "+values[1]);
                    console.log("Week Number : "+values[13]);
                    let date=(values[6].split(","));
                    
                    
+=======
+
+                //    current Time fixing
+                   Time=values[11].split("")
+                   for(i=0;i<11;i++)
+                   {
+                        if(i<=4)
+                        {
+                            curTime+=Time[i] 
+                        }
+                        else if(i>=9)
+                        {
+                            AmOrPm+=Time[i] 
+                        }
+>>>>>>> a24e157 (commit)
                     
+                   }
+
+
+                   Day=values[6].split(', ')
+                   
+                   document.getElementById("currentTime").innerText=curTime
+                   document.getElementById("timeZone").innerText="IN "+values[0].country_name;
+                   document.getElementById("day").innerText=Day[0];
+                   document.getElementById("weekNum").innerText=values[13];
+                   document.getElementById("yearTxt").innerText=values[14];
+                   document.getElementById("timeZone2").innerText=values[1];
+
+                   timingFunc(AmOrPm)
+
+                   console.log("Timezone : "+values[14]);                    
                 }
             )
             }
@@ -46,6 +81,58 @@ function getTime()
     
 }
 getTime();
+<<<<<<< HEAD
+=======
+
+
+
+function timingFunc(time)
+{
+    // time="PM"
+    if(time=='AM')
+    {
+        document.getElementsByClassName("wholeContainer")[0].id="light";
+        document.getElementById("light").style.backgroundImage="url('assets/desktop/bg-image-daytime.jpg')";
+        document.getElementsByClassName("wishTime")[0].innerText="Good Morning"
+        document.getElementById("timeImg").setAttribute("src","assets/desktop/icon-sun.svg")
+        document.getElementById("countryDetails").style.backgroundImage="radial-gradient(#ffffff,#c6c7c7d1)";
+        document.getElementById("countryDetails").style.color="black";
+    }
+    else if(time=='PM')
+    {
+        document.getElementsByClassName("wholeContainer")[0].id="dark";
+        document.getElementById("dark").style.backgroundImage="url('assets/desktop/bg-image-nighttime.jpg')";
+        document.getElementsByClassName("wishTime")[0].innerText="Good Evening"
+        document.getElementById("timeImg").setAttribute("src","assets/desktop/icon-moon.svg")
+        document.getElementById("countryDetails").style.backgroundImage="radial-gradient(#0e0e0e,#000000d1)";
+        document.getElementById("countryDetails").style.color="white";
+    }
+}
+
+            
+$(document).ready(function(){
+    var toggleCount=0
+    $(".more").click(function(){
+        if(toggleCount==0){
+
+            $(".div").css({'transition':'transform','transition-duration':'2s','transform':'translateY(-47%)'});
+            $(".more").html(`LESS <img src="assets/desktop/icon-arrow-up.svg" id="moonSun">`)
+            $(".more img").css("transform","rotate(180deg) translate(2px,3px)")
+            toggleCount++
+        }
+        else{
+
+            $(".div").css({'transition':'transform','transition-duration':'2s','transform':'translateY(0%)'});
+            $(".more").html(`MORE <img src="assets/desktop/icon-arrow-up.svg" id="moonSun">`)
+            $(".more img").css("transform","rotate(0deg) translate(2px,3px)")
+            toggleCount--
+        }
+    });
+});
+
+
+// qoute
+>>>>>>> a24e157 (commit)
 const randomquote=function(name){
     fetch(`https://api.api-ninjas.com/v1/quotes?category=${name}`,{
         method:'GET',
@@ -60,8 +147,21 @@ const randomquote=function(name){
        let quotes=data[0];
        let sentence=quotes.quote;
        let author=quotes.author
+<<<<<<< HEAD
       console.log(sentence);
       console.log(author);
  })
 }
  randomquote("success")
+=======
+       document.getElementById("quote").innerText=sentence
+       document.getElementsByClassName("author")[0].innerText="- "+author
+ })
+}
+ randomquote("success")
+
+ function randomQuote()
+ {
+    randomquote("success")
+ }
+>>>>>>> a24e157 (commit)
